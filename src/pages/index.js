844e27043ac671c36ca462ffcbbd4cf395998689
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-05-03 06:30:47 -0700

    meta image test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import About from "../../components/Home/About/About";
 import Body from "../../components/Home/Body/Body";
 import Footer from "../../components/Home/Footer/Footer";
@@ -6,7 +7,9 @@ import Header from "../../components/Home/Header/Header";
 export default function Home() {
   return (
     <>
-    
+    <Head>
+      <meta property="og:image:type" content="/thumbnail.png"/>
+    </Head>
       <Header/>
       <About/>
       <Footer/>
