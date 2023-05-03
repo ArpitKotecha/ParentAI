127bc6cc71aa7cee14b3ab1edd45f4bb4b68a288
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-05-03 06:38:49 -0700

    meta image test 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,10 @@ export default function Home() {
   return (
     <>
     <Head>
-      <meta property="og:image:type" content="/thumbnail.png"/>
+      <meta property="og:image" content="https://ibb.co/s6wwCg3"/>
+      <meta property="og:image:type" content="image/png"/>
+      <meta property="og:image:width" content="1024"/>
+      <meta property="og:image:height" content="1024"/>
     </Head>
       <Header/>
       <About/>
